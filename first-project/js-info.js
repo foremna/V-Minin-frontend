8117ae989d0e7d794f6c19304cbf16cc4a110720
Lada Penkova <whitethe2001@gmail.com>
2022-01-11 20:01:21 +0300

    Revert "Hello world"
    This reverts commit 7faa4d77bce5988c3195dc487bdb214dc1cd37b2.

--- first-project/js-info.js
+++
@@ -1,5 +0,0 @@
-alert('Кто разработал язык программирования JavaScript? = Брендан Эйх')
-alert('За сколько дней был разработан JavaScript? = 10 дней')
-alert('В каком году появился JavaScript? = в сентябре 1995 года')
-alert('JavaScript - это то же самое, что и Java? = JavaScript это не Java')
-alert('Как называется спецификация JavaScript? = ECMAScript')
