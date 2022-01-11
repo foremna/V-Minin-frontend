3b2de99711ce26f3a36d2a9cc2f13ce08f4ab775
Lada Penkova <whitethe2001@gmail.com>
2022-01-12 01:06:20 +0300

    Data types

---
+++ exercise1/index.js
@@ -0,0 +1,28 @@
+let name = 'Jessy'
+console.log('name', name, typeof name)
+
+let age = 20
+console.log('age', age, typeof age)
+
+let isSweetTea = true
+console.log('Is sweet tea?', isSweetTea, typeof isSweetTea)
+
+let variable;
+console.log('variable', variable, typeof variable)
+
+let bigInteger = 100n
+console.log('Big integer', bigInteger, typeof bigInteger)
+
+let symbolForObj = Symbol('id')
+console.log('Identification symbol for object', symbolForObj, typeof symbolForObj)
+
+let iDontKnowIsName = null
+console.log('Look, what is he doing?', iDontKnowIsName, typeof iDontKnowIsName)
+
+let car = {
+    brand: 'Toyota',
+    model: 'Soarer',
+    age: 1994
+}
+
+console.log('Car', car, typeof car)
---
+++ exercise2/index.js
@@ -0,0 +1,52 @@
+let nameCat = 'Potato'
+let age = 7
+let isGreenCat = false
+
+age = '7'
+nameCat = 123
+isGreenCat = null
+
+//
+
+let ageOfPerson1 = 18
+let ageOfPerson2 = '20'
+
+let nameOfAnimal1 = null
+let nameOfAnimal2 = 'Charlie'
+
+const bestProgrammingLanguage1 = 'JavaScript'
+const bestProgrammingLanguage2 = 'Java'
+
+let initialValue1
+let initialValue2 = 0
+
+let isJavaScriptProgrammer1 = true
+let isJavaScriptProgrammer2 = false
+
+let helloText1 = 'Hello!'
+let helloText2 = 'Привет!' 
+
+// 1
+ageOfPerson2 = ageOfPerson1
+console.log(ageOfPerson2, typeof ageOfPerson2)
+
+nameOfAnimal2 = nameOfAnimal1
+console.log(nameOfAnimal2, typeof nameOfAnimal2)
+
+initialValue2 = initialValue1
+console.log(initialValue2, typeof initialValue2)
+
+helloText2 = helloText1
+console.log(helloText2, typeof helloText2)
+
+// 2
+console.log('var является устаревшим')
+
+isJavaScriptProgrammer2 = isJavaScriptProgrammer1
+console.log(isJavaScriptProgrammer2, typeof isJavaScriptProgrammer2)
+
+// 3
+console.log('Переменную с const переопределить нельзя')
+
+console.log(bestProgrammingLanguage1, typeof bestProgrammingLanguage1)
+console.log(bestProgrammingLanguage2, typeof bestProgrammingLanguage2)
