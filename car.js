920748f4116ce7dd74326864c8bbc15e6f156462
Lada Penkova <whitethe2001@gmail.com>
2022-01-11 21:15:27 +0300

    Syntax

---
+++ car.js
@@ -0,0 +1,22 @@
+const carName = 'Cadillac'
+let maxCarSpeed = 320
+let carOwner = 'Jay'
+
+alert(carName)
+alert(maxCarSpeed)
+alert(carOwner)
+
+maxCarSpeed = 340
+carOwner = 'Jessy'
+console.log('У машины изменилась скорость', maxCarSpeed)
+console.log('У машины изменился владелец', carOwner)
+
+maxCarSpeed = 360
+carOwner = 'Andie'
+console.log('У машины изменилась скорость', maxCarSpeed)
+console.log('У машины изменился владелец', carOwner)
+
+maxCarSpeed = 380
+carOwner = 'Windie'
+console.log('У машины изменилась скорость', maxCarSpeed)
+console.log('У машины изменился владелец', carOwner)
---
+++ src/index.js
@@ -0,0 +1,17 @@
+// JavaScript код с созданием переменных
+/*
+Lada
+Jay
+*/
+
+let myName = 'Lada'
+let mySurname = 'Jay'
+let myFavoriteDrink = 'thyme tea'
+let myFavoriteAnimal = 'cat'
+let myFavoriteProgrammingLanguage = 'JavaScript & Vue'
+
+console.log(myName)
+console.log(mySurname)
+console.log(myFavoriteDrink)
+console.log(myFavoriteAnimal)
+console.log(myFavoriteProgrammingLanguage)
